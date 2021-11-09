a2fe68b1d86f402e44f5a9b88e554818d0d49375
Rafa Lozano <rafael@avena.io>
2021-11-08 22:22:07 -0600

    add new Patient class

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 import com.doctor.Doctor;
-import static ui.UiMenu.*;// se importa la clase completa
+import static ui.UiMenu.showMenu;// se importa la clase completa
 //static los metodos static se usa para llamarlos metodos sin necesidad de instancia la clase
 public class main {
     public static void main(String[] args) {
@@ -8,6 +8,9 @@ public class main {
 
         Doctor doctor2 = new Doctor();
         doctor2.showId();
+
+        Patient myPatient = new Patient("Rafa", "rafa.lozano.rolon");
+        System.out.println("El " + myPatient.name + " se registro con el correo: " + myPatient.email);
 
         showMenu();
 
